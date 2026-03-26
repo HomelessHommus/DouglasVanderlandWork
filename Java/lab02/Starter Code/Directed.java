feae88285e37d6d505750f7fa179cbc77261ac00
HomelessHommus <douglasvanderland@gmail.com>
2026-03-26 12:48:04 +1030

    starting checkpoint 1

---
+++ Java/lab02/Starter Code/Directed.java
@@ -0,0 +1,43 @@
+import java.util.List;
+
+public class Directed extends Graph{
+    @Override
+    void addVertex(Vertex v) {
+        
+    }
+
+    @Override
+    void addEdge(Vertex v, Vertex w) {
+
+    }
+
+    @Override
+    List<Vertex> adjacentTo(Vertex v) {
+        return null;
+    }
+
+    @Override
+    int degree(Vertex v) {
+        return 0;
+    }
+
+    @Override
+    List<Vertex> getVertices() {
+        return null;
+    }
+
+    @Override
+    boolean hasEdge(Vertex v, Vertex w) {
+        return false;
+    }
+
+    @Override
+    boolean hasVertex(Vertex vertex) {
+        return false;
+    }
+
+    @Override
+    Vertex getVertex(String v) {
+        return null;
+    }
+}
